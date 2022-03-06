fd88e117d91d1572d73e8938f0c24da2650d1984
pao.nattawut <pao.nattawut@bitkub.com>
2022-03-07 00:01:43 +0700

    fix css

--- src/components/each/Pagination/index.tsx
+++ src/components/each/Pagination/index.tsx
@@ -13,18 +13,18 @@ export const Pagination = (): JSX.Element => {
     return CardList.length >= 20 && count > 0 ? (
         <div className="bg-backgroundBlack px-4 py-3 flex items-center justify-between sm:px-6">
             <div className="flex-1 flex justify-between sm:hidden">
-                <a
-                    href="#"
-                    className="relative inline-flex items-center px-4 py-2  text-sm font-medium rounded-md text-white  hover:bg-gray-50"
+                <div
+                    onClick={() => paginationPage - 1 > 0 ? handlePagination(paginationPage - 1) : {}}
+                    className="relative inline-flex items-center px-4 py-2  text-sm font-medium rounded-md text-white "
                 >
                     Previous
-                </a>
-                <a
-                    href="#"
-                    className="ml-3 relative inline-flex items-center px-4 py-2  text-sm font-medium rounded-md text-white  hover:bg-gray-50"
+                </div>
+                <div
+                    onClick={() => handlePagination(paginationPage + 1)}
+                    className="ml-3 relative inline-flex items-center px-4 py-2  text-sm font-medium rounded-md text-white "
                 >
                     Next
-                </a>
+                </div>
             </div>
             <div className="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
                 <div>
